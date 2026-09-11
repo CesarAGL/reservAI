10346dd7d68ffde27fea4f94429b85c73beaf476
Cesar Augusto <xcesaraugustoz@gmail.com>
2026-09-11 10:06:21 -0300

    Add files via upload

--- BD/reservai.sql
+++ BD/reservai.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 19/08/2026 às 20:10
--- Versão do servidor: 8.4.8
+-- Tempo de geração: 11/09/2026 às 15:05
+-- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -28,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `quadras` (
-  `idQuadra` int NOT NULL,
-  `fotoQuadra` varchar(200) COLLATE utf8mb4_general_ci NOT NULL,
-  `nomeQuadra` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `esportes` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `localizacao` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `idQuadra` int(11) NOT NULL,
+  `fotoQuadra` varchar(200) NOT NULL,
+  `nomeQuadra` varchar(100) NOT NULL,
+  `esportes` varchar(100) NOT NULL,
+  `localizacao` varchar(100) NOT NULL,
   `horarioAbertura` time NOT NULL,
   `horarioFechamento` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -45,7 +45,8 @@ INSERT INTO `quadras` (`idQuadra`, `fotoQuadra`, `nomeQuadra`, `esportes`, `loca
 (1, 'assets/img/interiorQuadraIF2.jpg', 'Quadra IFPR', 'poliesportiva', 'PR-160, km 19,5 Jardim Bandeirantes, Telêmaco Borba - PR', '07:30:00', '22:00:00'),
 (2, 'assets/img/quadra_teste.jpg', 'Quadra Teste', 'Poliesportiva', 'Rua dos bobos, n°0', '08:00:00', '21:00:00'),
 (3, 'assets/img/furtadao.jpg', 'Furtadão', 'poliesportivo', 'Avenida das Nações, s/n - Centro, Telêmaco Borba', '09:00:00', '23:00:00'),
-(4, 'assets/img/vila_osorio.jpg', 'Quadra Vila Osório', 'Futsal e volêi', 'Vila Osório, perto da antiga Escola Dom Pedro.', '10:00:00', '22:00:00');
+(4, 'assets/img/vila_osorio.jpg', 'Quadra Vila Osório', 'Futsal e volêi', 'Vila Osório, perto da antiga Escola Dom Pedro.', '10:00:00', '22:00:00'),
+(5, 'assets/img/Revitalizacao-quadra-poliesportiva-do-CEU-Parque-Sao-Miguel.jpeg', 'quadra ficticia', 'poliesportiva', 'rua dos bobos, n°00', '10:00:00', '22:00:00');
 
 -- --------------------------------------------------------
 
@@ -54,26 +55,27 @@ INSERT INTO `quadras` (`idQuadra`, `fotoQuadra`, `nomeQuadra`, `esportes`, `loca
 --
 
 CREATE TABLE `reservas` (
-  `idReserva` int NOT NULL,
-  `idReservante` int NOT NULL,
-  `idQuadraReservada` int NOT NULL,
+  `idReserva` int(11) NOT NULL,
+  `idReservante` int(11) NOT NULL,
+  `idQuadraReservada` int(11) NOT NULL,
   `dataReserva` date NOT NULL,
   `horarioInicio` time NOT NULL,
   `horarioFim` time NOT NULL,
-  `statusReserva` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'agendado'
+  `esporteReserva` varchar(50) DEFAULT NULL,
+  `statusReserva` varchar(50) NOT NULL DEFAULT 'agendado'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `reservas`
 --
 
-INSERT INTO `reservas` (`idReserva`, `idReservante`, `idQuadraReservada`, `dataReserva`, `horarioInicio`, `horarioFim`, `statusReserva`) VALUES
-(2, 1, 2, '2026-08-24', '21:00:00', '23:00:00', 'agendado'),
-(4, 7, 1, '2026-09-09', '11:00:00', '13:00:00', 'agendado'),
-(6, 1, 1, '2026-09-09', '14:00:00', '16:00:00', 'cancelada'),
-(7, 7, 1, '2026-08-08', '10:00:00', '11:00:00', 'concluída'),
-(9, 7, 1, '2026-08-31', '12:00:00', '13:00:00', 'agendado'),
-(10, 1, 3, '2026-08-31', '17:00:00', '19:00:00', 'agendado');
+INSERT INTO `reservas` (`idReserva`, `idReservante`, `idQuadraReservada`, `dataReserva`, `horarioInicio`, `horarioFim`, `esporteReserva`, `statusReserva`) VALUES
+(2, 1, 2, '2026-08-24', '21:00:00', '23:00:00', 'Futsal', 'concluída'),
+(4, 7, 1, '2026-09-09', '11:00:00', '13:00:00', 'Volêi', 'concluída'),
+(6, 1, 1, '2026-09-09', '14:00:00', '16:00:00', 'Volêi', 'cancelada'),
+(7, 7, 1, '2026-08-08', '10:00:00', '11:00:00', 'Basquete', 'concluída'),
+(9, 7, 1, '2026-08-31', '12:00:00', '13:00:00', 'Futsal', 'concluída'),
+(10, 7, 1, '2026-09-28', '14:00:00', '16:00:00', 'Queimada', 'agendado');
 
 -- --------------------------------------------------------
 
@@ -82,14 +84,14 @@ INSERT INTO `reservas` (`idReserva`, `idReservante`, `idQuadraReservada`, `dataR
 --
 
 CREATE TABLE `usuarios` (
-  `idUsuario` int NOT NULL,
-  `cpfUsuario` char(11) COLLATE utf8mb4_general_ci NOT NULL,
-  `nomeUsuario` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  `cpfUsuario` char(11) NOT NULL,
+  `nomeUsuario` varchar(100) NOT NULL,
   `dataNascimentoUsuario` date NOT NULL,
-  `telefoneUsuario` char(11) COLLATE utf8mb4_general_ci NOT NULL,
-  `emailUsuario` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `senhaUsuario` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
-  `nivelUsuario` varchar(15) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'usuario'
+  `telefoneUsuario` char(11) NOT NULL,
+  `emailUsuario` varchar(100) NOT NULL,
+  `senhaUsuario` varchar(100) NOT NULL,
+  `nivelUsuario` varchar(15) NOT NULL DEFAULT 'usuario'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -97,9 +99,9 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`idUsuario`, `cpfUsuario`, `nomeUsuario`, `dataNascimentoUsuario`, `telefoneUsuario`, `emailUsuario`, `senhaUsuario`, `nivelUsuario`) VALUES
-(1, '13300080900', 'Cesar Augusto', '2008-05-02', '42998069904', '20241tbor0020025@estudantes.ifpr.edu.br', '281d5cbef8ded4e9bee409e3b9c67ab2', 'administrador'),
-(3, '48260112965', 'Miguel Gustavo', '2009-04-02', '42998643553', 'miguelgustavovm27@gmail.com', 'a5aad544b38a088b35b395645efe0d61', 'usuario'),
-(4, '36528291900', 'Alan de Souza Vanes', '2008-06-06', '66996309666', 'baixinhodesouzaa@gmail.com', '564f4bcd11273b8ea6b49fbe2dc2ad1c', 'usuario'),
+(1, '13300080900', 'Cesar Augusto', '2008-05-02', '42998069904', 'cesaraugusto@gmail.com', '281d5cbef8ded4e9bee409e3b9c67ab2', 'administrador'),
+(3, '48260112965', 'Miguel Gustavo', '2009-04-02', '42998643553', 'miguelgustavo@gmail.com', 'a5aad544b38a088b35b395645efe0d61', 'usuario'),
+(4, '36528291900', 'Alan de Souza Vanes', '2008-06-06', '66996309666', 'alan@gmail.com', '564f4bcd11273b8ea6b49fbe2dc2ad1c', 'usuario'),
 (5, '97194506904', 'Fulano', '2001-11-07', '42900000000', 'fulano@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 'usuario'),
 (6, '60533326907', 'ciclano', '1998-02-21', '42933333333', 'ciclano@gmail.com', '4a7d1ed414474e4033ac29ccb8653d9b', 'usuario'),
 (7, '04533601901', 'Júlio', '2004-10-08', '42987654321', 'julio@gmail.com', '113180fa10fcf7a118ecdbcd21c4cd24', 'usuario');
@@ -137,19 +139,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `quadras`
 --
 ALTER TABLE `quadras`
-  MODIFY `idQuadra` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idQuadra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de tabela `reservas`
 --
 ALTER TABLE `reservas`
-  MODIFY `idReserva` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idReserva` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `idUsuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restrições para tabelas despejadas
